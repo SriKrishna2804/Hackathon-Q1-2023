32f9cf0b91a12b8a10663ca475c1c6c8299ee088
NUNNA Sri Krishna <sri.krishna.nunna@hexagon.com>
2023-04-04 22:05:19 +0530

    Added few UI elements for Login Page.

--- src/components/Navigation/LeftNavBar.tsx
+++ src/components/Navigation/LeftNavBar.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import styles from 'mdsreact/styles/Home.module.css'
+import { TextField, Typography } from "@mui/material";
+import Image from 'next/image'
 
 export default function Login() {
 
@@ -11,6 +13,23 @@ export default function Login() {
   });
 
   return (
-    <Box className={styles.login} />
+    <Box className={styles.login} > 
+      <Image src="/mds/Multivista-Logo-190x190.png" alt="Multivista Logo" width="190" height="190" />
+      <Typography variant="h3" component="h3" className={styles.title}>
+        CLIENT LOGIN
+      </Typography>;
+
+      <TextField
+          className="styles.username"
+          id="user-name-required"
+          label="USER NAME"
+        />
+     <TextField
+          id="outlined-password-input"
+          label="PASSWORD"
+          type="password"
+          autoComplete="current-password"
+        />
+    </Box>
   );
 }
