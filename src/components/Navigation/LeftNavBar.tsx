92a4dada582ae3b47ae8eccf71c0c7830486e489
NUNNA Sri Krishna <sri.krishna.nunna@hexagon.com>
2023-04-04 18:12:09 +0530

    Added UI Layouts

---
+++ src/components/Navigation/LeftNavBar.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import styles from 'mdsreact/styles/Home.module.css'
+
+export default function Login() {
+
+  React.useEffect(() => {
+    const func = async () => {
+      
+    };
+  });
+
+  return (
+    <Box className={styles.login} />
+  );
+}
