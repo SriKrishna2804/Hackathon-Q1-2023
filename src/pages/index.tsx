b9597cb418aa84e9492f2a8c1ed2cdfeeb8fb537
NUNNA Sri Krishna <sri.krishna.nunna@hexagon.com>
2023-04-05 17:58:50 +0530

    Added slideshow data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,60 @@ import styles from "mdsreact/styles/home.module.css";
 import { Grid, Box, Container, Skeleton, Typography } from "@mui/material";
 import LeftSidebar from "mdsreact/components/login/left-sidebar";
 import Layout from "mdsreact/components/layout";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  let photos = [
+    {
+      url: "/hexagon-live2023.jpg",
+      title: "JOIN US AT HXGN LIVE GLOBAL 2023 JUNE 12-15, 2023",
+      subTitle:
+        "Join us in Las Vegas for the latest construction tech, big keynotes, and to network with experts and thought leaders in your industry.",
+      link: "https://cvent.me/VdzgDm?rt=e3KDK79YOU-rYO1h7RLvNg&RefId=Website_GEO_Multivista",
+    },
+    {
+      url: "/blk-banner-20190510.jpg",
+      title: "INTRODUCING 3D IMAGES",
+      subTitle: "VIEW A PICTURE, MEASURE ANYTHING.",
+      link: "https://www.multivista.com/measurable-3d-images/",
+    },
+    {
+      url: "/uav-hero-v180930.jpg",
+      title: "MULTIVISTA UAV",
+      subTitle: "IT'S NOT SCIENCE FICTION IT'S YOUR COMPETITIVE ADVANTAGE",
+      link: "https://www.multivista.com/services/construction-drone-uav/",
+    },
+    {
+      url: "/hero-webcam-181103.jpg",
+      title: "WEBCAM SERVICES",
+      subTitle: "LIVE FEEDS, ARCHIVED IMAGES, TIMELAPSE, AND PTZ POSITIONING",
+      link: "https://www.multivista.com/services/construction-drone-uav/",
+    },
+    {
+      url: "/bg-firestopping.jpg",
+      title: "AUTOMATED FIRESTOPPING ASSESSMENTS",
+      subTitle:
+        "Our automated system detects unsealed penetrations in 2D and 360 images and generates a punch list for the contractor team, saving hundreds of hours and thousands of dollars in rework.",
+      link: "https://www.multivista.com/firestopping",
+    },
+    {
+      url: "/help-200330.jpg",
+      title: "STAY SAFE, WORK REMOTELY AND REMAIN ON-SCHEDULE",
+      subTitle:
+        "We can help. Multivista will rapidly provide the actionable insights you need to manage your projects remotely, reduce risk and liability, and minimize rework.",
+      link: "https://info.multivista.com/schedule-a-demo",
+    },
+    {
+      url: "/integrations-v190214.jpg",
+      title: "BETTER TOGETHER",
+      subTitle: "TEAR DOWN THE DATA SILOS",
+      link: "https://www.multivista.com/about/integrations/",
+    },
+  ];
+
   return (
     <Layout>
       <Head>
@@ -20,11 +70,56 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Grid container className={styles.main}> 
+      <Grid container className={styles.main}>
         <LeftSidebar />
         <Grid item xs={9} display={{ xs: "none", lg: "block" }}>
           <Container className={styles.container}>
-            {/* Add Sliding Images Component */}
+            <Carousel autoPlay showThumbs={false} infiniteLoop>
+              <div className={styles.divCenter}>
+                <img alt={photos[0].title} src={photos[0].url} />
+                <div className={styles.leftCentered}>
+                  <h3 className={styles.imageTitle}>{photos[0].title}</h3>
+                  <h4 className={styles.imageTitle}>{photos[0].subTitle}</h4>
+                  <a
+                    target="_blank"
+                    href={photos[0].link}
+                    rel="noopener noreferrer"
+                  >
+                    <div className={`${styles.btn} ${styles.warning}`} />
+                  </a>
+                  {/* <p>{photos[0].link}</p> */}
+                  {/* <Link href={photos[0].link} className={`${styles.btn} ${styles.warning}`} passHref> */}
+                  {/* <a target='_blank' rel="noopener">Learn More</a> */}
+                  {/* Learn More
+              </Link> */}
+                  {/* <button className={`${styles.btn} ${styles.warning}`}  onClick={() => router.push(photos[0].link)}>Learn More</button> */}
+                </div>
+              </div>
+              <div>
+                <img alt={photos[1].title} src={photos[1].url} />
+                <p className="legend">Legend 2</p>
+              </div>
+              <div>
+                <img alt={photos[2].title} src={photos[2].url} />
+                <p className="legend">Legend 2</p>
+              </div>
+              <div>
+                <img alt={photos[3].title} src={photos[3].url} />
+                <p className="legend">Legend 2</p>
+              </div>
+              <div>
+                <img alt={photos[4].title} src={photos[4].url} />
+                <p className="legend">Legend 2</p>
+              </div>
+              <div>
+                <img alt={photos[5].title} src={photos[5].url} />
+                <p className="legend">Legend 2</p>
+              </div>
+              <div>
+                <img alt={photos[6].title} src={photos[6].url} />
+                <p className="legend">Legend 2</p>
+              </div>
+            </Carousel>
           </Container>
         </Grid>
       </Grid>
